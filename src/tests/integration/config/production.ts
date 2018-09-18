ee3331d88db57c17a292275be52fceeccae58456
Kevin Greene <kevin.greene@creditkarma.com>
2018-09-18 12:54:43 -0700

    chore: Add more testing around nested objects

--- src/tests/integration/config/production.ts
+++ src/tests/integration/config/production.ts
@@ -8,7 +8,10 @@ export const server = {
 }
 
 export const database = {
-    username: '${TEST_USERNAME}',
+    username: {
+        _source: 'env',
+        _key: 'TEST_USERNAME',
+    },
     password: {
         _source: 'env',
         _key: 'TEST_PASSWORD',
